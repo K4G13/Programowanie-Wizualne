2a6ce5df74c501fbb88d9d410bdb1016a0ecdb9a
“k4g13” <polcyn.piotrek@gmail.com>
2024-01-30 05:39:15 +0100

    4 buttons for phones working

--- PhonesApp/PhonesAppMAUI/ViewModels/PhonesCollectionViewModel.cs
+++ PhonesApp/PhonesAppMAUI/ViewModels/PhonesCollectionViewModel.cs
@@ -11,14 +11,18 @@ using static Microsoft.Maui.ApplicationModel.Permissions;
 
 namespace PhonesAppMAUI.ViewModels
 {
-    public partial class PhonesCollectionViewModel: ObservableObject
+    public partial class PhonesCollectionViewModel : ObservableObject
     {
         [ObservableProperty]
         private ObservableCollection<PhoneViewModel> phones = new ObservableCollection<PhoneViewModel>();
 
         private BLC.BLC blc;
 
-        public PhonesCollectionViewModel(BLC.BLC blc) 
+
+        [ObservableProperty]///???
+        private IEnumerable<Core.DisplayType> displayTypesValues;
+
+        public PhonesCollectionViewModel(BLC.BLC blc)
         {
             this.blc = blc;
             IEnumerable<IPhone> phonesDB = blc.GetPhones();
@@ -27,8 +31,13 @@ namespace PhonesAppMAUI.ViewModels
                 phones.Add(new PhoneViewModel(phone));
             }
 
+            ///??
+            DisplayTypesValues = Enum.GetValues(typeof(Core.DisplayType)).Cast<Core.DisplayType>();
+
+
             IsEditing = false;
             PhoneEdit = null;
+            SelectedPhoneBuffer = null;
         }
 
         [ObservableProperty]
@@ -36,6 +45,7 @@ namespace PhonesAppMAUI.ViewModels
 
         [ObservableProperty]
         private bool isEditing;
+
 
         [RelayCommand(CanExecute = nameof(CanCreateNewPhone))]
         private void CreateNewPhone()
@@ -48,28 +58,77 @@ namespace PhonesAppMAUI.ViewModels
 
         private bool CanCreateNewPhone() => !IsEditing;
 
+
         [RelayCommand(CanExecute = nameof(CanEditBeSaved))]
         private void SavePhone()
         {
-            Phones.Add(phoneEdit);
+
+            if (SelectedPhone != null)
+            {
+                Phones.Remove(SelectedPhone);
+                blc.DeletePhone(SelectedPhone);
+            }
+
+            Phones.Add(PhoneEdit);
+            blc.SavePhone(PhoneEdit);
+
             PhoneEdit.PropertyChanged -= OnPhoneEditPropertyChanged;
             PhoneEdit = null;
+            SelectedPhoneBuffer = null;
             IsEditing = false;
             RefreshCanExecute();
         }
-        private bool CanEditBeSaved() => PhoneEdit != null &&
-                                            PhoneEdit.Name != null &&
-                                            PhoneEdit.ID >= 0;
+        private bool CanEditBeSaved() => PhoneEdit != null && PhoneEdit.Name != null && PhoneEdit.ID >= 0;
+
 
         [RelayCommand(CanExecute = nameof(CanEditBeCanceled))]
         private void CancelEdit()
         {
             PhoneEdit.PropertyChanged -= OnPhoneEditPropertyChanged;
             PhoneEdit = null;
+            SelectedPhoneBuffer = null;
             IsEditing = false;
             RefreshCanExecute();
         }
         private bool CanEditBeCanceled() => isEditing;
+
+
+        private PhoneViewModel SelectedPhoneBuffer = null;
+        public PhoneViewModel SelectedPhone
+        {
+            get => SelectedPhoneBuffer;
+            set { if (value != null && !isEditing) selectPhone(value); }
+        }
+        private void selectPhone(PhoneViewModel refPhone)
+        {
+            SelectedPhoneBuffer = refPhone;
+
+            PhoneEdit = new PhoneViewModel(blc);
+            PhoneEdit.ID = refPhone.ID;
+            PhoneEdit.Producer = refPhone.Producer;
+            PhoneEdit.Name = refPhone.Name;
+            PhoneEdit.DiagonalScreenSize = refPhone.DiagonalScreenSize;
+            PhoneEdit.DisplayType = refPhone.DisplayType;
+
+            PhoneEdit.PropertyChanged += OnPhoneEditPropertyChanged;
+            IsEditing = true;
+            RefreshCanExecute();
+        }
+
+        [RelayCommand(CanExecute = nameof(CanDeletePhone))]
+        private void DeletePhone()
+        {
+            Phones.Remove(SelectedPhone);
+            blc.DeletePhone(SelectedPhone);
+
+            PhoneEdit.PropertyChanged -= OnPhoneEditPropertyChanged;
+            PhoneEdit = null;
+            SelectedPhoneBuffer = null;
+            IsEditing = false;
+            RefreshCanExecute();
+        }
+        private bool CanDeletePhone() => isEditing && SelectedPhoneBuffer!=null;
+
 
         private void OnPhoneEditPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
@@ -81,6 +140,7 @@ namespace PhonesAppMAUI.ViewModels
             CreateNewPhoneCommand.NotifyCanExecuteChanged();
             SavePhoneCommand.NotifyCanExecuteChanged();
             CancelEditCommand.NotifyCanExecuteChanged();
+            DeletePhoneCommand.NotifyCanExecuteChanged();
         }
 
     }
